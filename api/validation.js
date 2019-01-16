a37b84a12412e80ece353b465904e176f28eea94
RafaDraw <www.rafadraw@gmail.com>
2019-01-16 16:58:04 -0200

    App Validation

---
+++ api/validation.js
@@ -0,0 +1,23 @@
+module.exports = app => {
+    const existsOrError = (value, msg) => {
+        if (!value) throw msg;
+        if (Array.isArray(value) && value.length === 0) throw msg;
+        if (typeof value === 'string' && !value.trim()) throw msg;
+    };
+
+    const notExistisOrError  = (value, msg) => {
+        try {
+            existsOrError(value, msg);
+        } catch (msg) {
+            return
+        }
+
+        throw msg;
+    };
+
+    const equalsOrError = (valueA, valueB, msg) => {
+        if (valueA !== valueB) throw msg
+    };
+
+    return { existsOrError, notExistisOrError, equalsOrError }
+};
