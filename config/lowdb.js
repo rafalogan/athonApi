e32d56ba5c5a3ad004facbcf2ae9d498975b2886
RafaDraw <www.rafadraw@gmail.com>
2021-04-12 16:51:25 -0300

    Refactor init

--- config/lowdb.js
+++
@@ -1,7 +0,0 @@
-const lowDb = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-
-const adapter = new FileSync('./service/stats_db.json');
-const statsdb = lowDb(adapter);
-
-module.exports = statsdb;
