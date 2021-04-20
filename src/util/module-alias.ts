a2ebd948a428981035fe6c41c17294b3a4843adc
RafaDraw <www.rafadraw@gmail.com>
2021-04-20 17:12:31 -0300

    refactor alias

--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -2,9 +2,8 @@ import path from 'path';
 import moduleAlias from 'module-alias';
 
 const files = path.resolve(__dirname, '../..');
-const alias = {
+
+moduleAlias.addAliases({
 	src: path.join(files, 'src'),
 	test: path.join(files, 'test'),
-};
-
-moduleAlias.addAliases(alias);
+});
