d97a5325707075a72b91c489e6b9a1c00086f187
RafaDraw <www.rafadraw@gmail.com>
2021-04-23 13:13:26 -0300

    add test #KnexConfig

---
+++ src/config/knex.config.spec.ts
@@ -0,0 +1,23 @@
+import KnexConfig from 'src/config/knex.config';
+import MockProfile from 'database/tests/mocks/mock-profile.json';
+import MockKnexFile from 'database/tests/mocks/mock-knex-file.json';
+import { IProfile } from 'src/environment/types/profile';
+
+describe('#KenexConfig Suite', () => {
+	let profile: IProfile;
+
+	beforeEach(() => {
+		profile = MockProfile;
+
+		jest.clearAllMocks();
+	});
+
+	test('Should', () => {
+		const knexConfig = new KnexConfig(profile);
+		const expected = MockKnexFile;
+
+		const result = knexConfig.getFile();
+
+		expect(result).toStrictEqual(expected);
+	});
+});
