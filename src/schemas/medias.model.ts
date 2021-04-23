e7d19516a570138ffbedb6f09f6f6da5ec708ed5
RafaDraw <www.rafadraw@gmail.com>
2021-04-23 12:42:24 -0300

    Create Schemas to MongoDB

---
+++ src/schemas/medias.model.ts
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+
+const MediasSchema = new Schema(
+	{
+		file: {
+			type: Schema,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			maxlength: 1000,
+			required: false,
+		},
+		type: {
+			type: String,
+			required: true,
+		},
+		userId: {
+			type: Number,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const MediasModel = model('Medias', MediasSchema);
+
+export default MediasModel;
