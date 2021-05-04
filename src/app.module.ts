246b809e53aa3db26a2f21ec5dc70d4ec7fcc914
RafaDraw <www.rafadraw@gmail.com>
2021-05-04 20:22:26 -0300

    Create AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,46 @@
+import { Application } from 'express';
 import http from 'http';
 import https from 'https';
 
-export default class AppModule {}
+import LogHandler from 'src/core/handlers/log.handler';
+import { IHttpsOptions } from 'src/environment/types/https-options';
+
+export default class AppModule {
+	express: Application;
+	httpsOptions: IHttpsOptions;
+	port: number;
+	enableHTTPS: boolean;
+	baseURL: string;
+
+	constructor(private log: LogHandler, express: Application, options: IHttpsOptions, baseURL: string, port: number, enableHTTPS: boolean) {
+		this.express = express;
+		this.httpsOptions = options;
+		this.port = port;
+		this.enableHTTPS = enableHTTPS;
+		this.baseURL = baseURL;
+	}
+
+	createServer() {
+		this.enableHTTPS ? this._createHttpServer() : this._createHttpsServer();
+	}
+
+	private _createHttpServer() {
+		http.createServer(this.express).listen(this.port).on('listening', this._onServerUp.bind(this)).on('error', this._onServerError.bind(this));
+	}
+
+	private _createHttpsServer() {
+		https
+			.createServer(this.httpsOptions, this.express)
+			.listen(this.port)
+			.on('listening', this._onServerUp.bind(this))
+			.on('error', this._onServerError.bind(this));
+	}
+
+	private _onServerUp() {
+		this.log.http(`SERVER ONLINE: ${this.baseURL}`);
+	}
+
+	private _onServerError(error: NodeJS.ErrnoException) {
+		this.log.error(`ERROR: On Server Inti: ${__filename}`, error);
+	}
+}
