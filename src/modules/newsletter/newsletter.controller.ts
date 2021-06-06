59758a0ecbf635b9559b7e5eac22a9e62ff7615f
RafaDraw <www.rafadraw@gmail.com>
2021-06-06 18:16:17 -0300

    Create NewsletterController.save

--- src/modules/newsletter/newsletter.controller.ts
+++ src/modules/newsletter/newsletter.controller.ts
@@ -2,13 +2,23 @@ import { Request, Response } from 'express';
 
 import { AbstractController, ResponseController } from 'src/core/controller';
 import { NewsletterService } from 'src/services';
+import { Newsletter } from 'src/entities';
 
 export class NewsletterController extends AbstractController {
 	constructor(private newsletterService: NewsletterService, private response: ResponseController) {
 		super();
 	}
 
-	save(req: Request, res: Response) {}
+	save(req: Request, res: Response) {
+		const data = this.newsletterService.fieldsFilter(req.body);
+
+		if (!(data instanceof Newsletter)) return this.response.onError(res, data.message, data);
+
+		this.newsletterService
+			.create(data)
+			.then(result => this.response.onSuccess(res, result))
+			.catch(err => this.response.onError(res, 'unexpected error', { err }));
+	}
 
 	edit(req: Request, res: Response) {}
 
