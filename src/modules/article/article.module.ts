ff841d37a72abec0ce66285b77515aa4900db9d5
RafaDraw <www.rafadraw@gmail.com>
2021-06-16 17:01:00 -0300

    Create ArticlesModule

---
+++ src/modules/article/article.module.ts
@@ -0,0 +1,19 @@
+import { Application } from 'express';
+import { ArticleService, AuthService } from 'src/services';
+import { ResponseController } from 'src/core/controller';
+import { ArticleController } from 'src/modules/article/article.controller';
+import { ArticleRouter } from 'src/modules/article/article.router';
+
+export default class ArticleModule {
+	private articleController: ArticleController;
+	private articleRouter: ArticleRouter;
+
+	constructor(articleService: ArticleService, responseController: ResponseController, app: Application, auth: AuthService) {
+		this.articleController = new ArticleController(articleService, responseController);
+		this.articleRouter = new ArticleRouter(this.articleController, app, auth);
+	}
+
+	init() {
+		return this.articleRouter.exec();
+	}
+}
