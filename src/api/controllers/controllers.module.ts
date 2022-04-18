17a9f4bc5bb427e1b4abc580fbc9ea5e5f6800e8
Rafael Candeira <rafaelcandeira@MacBook-Pro-de-Rafael.local>
2022-04-18 15:19:39 -0300

    filx instance Profile controller and profile roule controller

--- src/api/controllers/controllers.module.ts
+++ src/api/controllers/controllers.module.ts
@@ -2,15 +2,21 @@ import { ServicesModule } from 'src/services';
 import { LoginController } from 'src/api/controllers/login.controller';
 import { UserController } from 'src/api/controllers/user.controller';
 import { RuleController } from 'src/api/controllers/rule.controller';
+import { ProfileController } from 'src/api/controllers/profile.controller';
+import { ProfileRuleController } from 'src/api/controllers/profile-rule.controller';
 
 export class ControllersModule {
 	loginController: LoginController;
 	userController: UserController;
 	ruleController: RuleController;
+	profileController: ProfileController;
+	profileRuleController: ProfileRuleController;
 
 	constructor(private servers: ServicesModule) {
 		this.loginController = new LoginController(this.servers.loginService);
 		this.userController = new UserController(this.servers.userService);
 		this.ruleController = new RuleController(this.servers.ruleService);
+		this.profileController = new ProfileController(this.servers.profileService);
+		this.profileRuleController = new ProfileRuleController(this.servers.profileRuleService);
 	}
 }
